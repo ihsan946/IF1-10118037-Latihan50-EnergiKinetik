b4e799e6e39dd07e4fc98040a696a310d088b94c
Muhammad Ihsan <55564001+ihsan946@users.noreply.github.com>
2019-10-20 10:42:17 +0700

    Add files via upload

---
+++ IF1-10118037-Latihan50-EnergiKinetik/src/Main/main.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Main;
+
+/**
+ *
+ * @author Muhammad Ihsan
+ * NAMA       : Muhammad Ihsan
+ * KELAS     : IF-1
+ * NIM          : 10118037
+ * Deskripsi Program   : Program ini berisi program menghitung energi kinetik
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        EnergiKinetik ek = new EnergiKinetik();
+        ek.tampilHasil();
+    }
+    
+}
